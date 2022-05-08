89077c9060a570d45da0821afa15a89e03b46798
MihailRoot <47941033+MihailRoot@users.noreply.github.com>
2022-05-08 15:41:13 -0700

    Initial commit

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody body;
+    public float speed;
+    public float rotationSpeed;
+    private float vertical, horizontal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        speed = 100f;
+        rotationSpeed = 50f;
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        //body.velocity = (transform.forward * vertical) * speed * Time.fixedDeltaTime;
+        body.velocity = (-transform.up * vertical) * speed * Time.fixedDeltaTime;
+        transform.Rotate((transform.forward * horizontal) * rotationSpeed * Time.fixedDeltaTime);
+
+       // transform.Rotate((transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime);
+    }
+}
