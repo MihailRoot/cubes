11ed5ef551b9e722375e20390ab0d340f8ec7ea7
MihailRoot <47941033+MihailRoot@users.noreply.github.com>
2022-05-08 16:18:47 -0700

    f

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -26,7 +26,7 @@ public class player : MonoBehaviour
         //body.velocity = (transform.forward * vertical) * speed * Time.fixedDeltaTime;
         body.velocity = (-transform.up * vertical) * speed * Time.fixedDeltaTime;
         transform.Rotate((transform.forward * horizontal) * rotationSpeed * Time.fixedDeltaTime);
-//
+////
        // transform.Rotate((transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime);
     }
 }
