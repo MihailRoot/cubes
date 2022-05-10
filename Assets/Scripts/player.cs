11343f4ebe2042704bf4343658aea1bde2628aa0
MihailRoot <47941033+MihailRoot@users.noreply.github.com>
2022-05-10 13:56:46 -0700

    Update(gravity,jumps)
    UPDATE!!!

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,6 +9,10 @@ public class player : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     private float vertical, horizontal;
+    public bool is_groud = true;
+    public float jump = 20;
+
+    public float gravityforce = 5;
     
     // Start is called before the first frame update
     void Start()
@@ -19,14 +23,22 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        speed = 100f;
+        speed = 30f;
         rotationSpeed = 50f;
         vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
         //body.velocity = (transform.forward * vertical) * speed * Time.fixedDeltaTime;
         body.velocity = (-transform.up * vertical) * speed * Time.fixedDeltaTime;
         transform.Rotate((transform.forward * horizontal) * rotationSpeed * Time.fixedDeltaTime);
-        // transform.Rotate((transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTime
-        
+        // transform.Rotate((transform.up * horizontal) * rotationSpeed * Time.fixedDeltaTim
+        body.AddForce(Physics.gravity * (gravityforce - 1) *body.mass);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && is_groud)
+        {
+            body.AddForce(Vector3.up * jump,ForceMode.Impulse);
+        }
     }
 }
