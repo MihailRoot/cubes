cad14161f272438b011081e4b953cf3d5f3002c1
MihailRoot <47941033+MihailRoot@users.noreply.github.com>
2022-05-12 12:30:44 -0700

    UPDATE TIMER and finish game(I'll update it soon)

--- Assets/Scripts/block.cs
+++ Assets/Scripts/block.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class block : MonoBehaviour
 {
+    public bool level = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    private void OnTriggerEnter(Collider other)
+    }
+    
+     void OnTriggerEnter(Collider other)
     {
+        //Активируем bool level
         Debug.Log("Молодец");
-        Application.Quit();
+        level = true;
+    }
+    void OnGUI()
+    {
+        //Проверяем завершение lv и выводим gui
+        if (level == true)
+        {
+            Debug.Log(level);
+            GUI.Label(new Rect(400, 200, 200, 50), "ПЕРВЫЙ УРОВЕНЬ ПРОЙДЕТ!");
+            new System.Timers.Timer(10000);
+            Application.Quit();
+        }
+
     }
     // Update is called once per frame
     void Update()
